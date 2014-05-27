f0bb8b35a6f482097fff1c8f4d32e11b1de051ee
Matt Duff <matt.duff@moneydesktop.com>
2014-05-27 07:06:58 -0600

    added Matchismo project for Stanford course

---
+++ Matchismo/Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Duff on 5/27/14.
+//  Copyright (c) 2014 tiniestTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
